9d337f29754bc352bd288a0563b672aa60a992d2
Peter Krevenets <peter.k.nets@gmail.com>
2017-10-10 10:09:23 +0300

    Create basic Header component

---
+++ slinker_web/src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+/**
+ * Voyage-2 Team-Bears-23
+ * @date 10/10/2017
+ *
+ * @ticket SLNKR-017
+ * @link https://github.com/chingu-coders/Voyage2-Bears-23/issues/14
+ *
+ * @flow
+ */
+import React from 'react'
+
+type Props = {
+};
+
+const Header = ({}: Props) => (
+  <div>Header</div>
+);
+
+export default Header;
