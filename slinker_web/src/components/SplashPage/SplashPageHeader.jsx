4fa1f653ea6fc913510d441e38dc99c81c4322d3
Peter Krevenets <peter.k.nets@gmail.com>
2017-10-25 19:14:16 +0300

    Create basic components for Splash Page

---
+++ slinker_web/src/components/SplashPage/SplashPageHeader.jsx
@@ -0,0 +1,20 @@
+/**
+ * Voyage-2 Team-Bears-23
+ * @date 25/10/2017
+ *
+ * @ticket SLNKR-020
+ * @link https://github.com/chingu-coders/Voyage2-Bears-23/issues/20
+ *
+ * @flow
+ */
+
+import React from 'react';
+
+const SplashPageHeader = () => (
+  <div className="o-splash-header">
+    <div className="header-logo">Slinker</div>
+    <div className="header-button">Add to Slask</div>
+  </div>
+);
+
+export default SplashPageHeader;
---
+++ slinker_web/src/components/SplashPage/index.jsx
@@ -0,0 +1,24 @@
+/**
+ * Voyage-2 Team-Bears-23
+ * @date 25/10/2017
+ *
+ * @ticket SLNKR-020
+ * @link https://github.com/chingu-coders/Voyage2-Bears-23/issues/20
+ *
+ * @flow
+ */
+
+import React from 'react';
+import SplashPageHeader from './SplashPageHeader';
+import SplashPageMain from './SplashPageMain';
+import SplashPageFooter from './SplashPageFooter';
+
+const SplashPage = () => (
+  <div>
+    <SplashPageHeader />
+    <SplashPageMain />
+    <SplashPageFooter />
+  </div>
+);
+
+export default SplashPage;
